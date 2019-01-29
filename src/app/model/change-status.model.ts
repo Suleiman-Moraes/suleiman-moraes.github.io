459708eced9fa54053c387bcf096dc96d93d3cd5
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-01-29 16:05:30 -0200

    Angulão

--- src/app/model/change-status.model.ts
+++
@@ -1,7 +0,0 @@
-import { User } from 'src/app/model/user.model';
-
-export class ChangeStatus {
-    public userChange: User;
-    public dataAlteracao: string;
-    public statusEnum: string;
-}
--- src/app/model/summary.model.ts
+++
@@ -1,8 +0,0 @@
-export class Summary{
-    public amountNovo: number;
-    public amountResolvido: number;
-    public amountAprovado: number;
-    public amountDesaprovado: number;
-    public amountDesignado: number;
-    public amountFechado: number;
-}
